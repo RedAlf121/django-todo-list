4ec9fc6941ed7b5c2b1cfe6f77125cbdd081017d
Alfredo Hernández Rodríguez <111950884+RedAlf121@users.noreply.github.com>
2024-12-21 13:39:45 -0500

    Task structure done

--- Task/models.py
+++ Task/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=50),
+    description = models.TextField(blank=True),
+    date = models.DateField(),
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
---
+++ Task/services/task_services.py
@@ -0,0 +1,17 @@
+from models import Task
+from django.contrib.auth.models import User
+
+def getTask(user: User, id: int):
+    pass
+
+def listTask(user: User):
+    pass
+
+def addTask():
+    pass
+
+def updateTask(user: User, task: Task, id: int):
+    pass
+
+def deleteTask(user: User, id: int):
+    pass
